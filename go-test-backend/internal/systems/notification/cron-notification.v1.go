a1ac2e0a9f0b8b5d7782e1f63a1e0af282bb3bcd
aquirius <maxi.guth@gmx.net>
2024-08-29 17:04:58 +0200

    feat: boilderplate cron notifications

--- go-test-backend/internal/systems/notification/cron-notification.v1.go
+++ go-test-backend/internal/systems/notification/cron-notification.v1.go
@@ -74,23 +74,208 @@ type Status struct {
 	WaterTempMax float64 `db:"water_temp_max"`
 }
 
+type Crop struct {
+	CropID       int     `db:"id"`
+	CUID         uint64  `db:"cuid"`
+	CropName     string  `db:"crop_name"`
+	AirTempMin   float64 `db:"air_temp_min"`
+	AirTempMax   float64 `db:"air_temp_max"`
+	HumidityMin  float64 `db:"humidity_min"`
+	HumidityMax  float64 `db:"humidity_max"`
+	PHLevelMin   float64 `db:"ph_level_min"`
+	PHLevelMax   float64 `db:"ph_level_max"`
+	OrpMin       float64 `db:"orp_min"`
+	OrpMax       float64 `db:"orp_max"`
+	TdsMin       uint16  `db:"tds_min"`
+	TdsMax       uint16  `db:"tds_max"`
+	WaterTempMin float64 `db:"water_temp_min"`
+	WaterTempMax float64 `db:"water_temp_max"`
+}
+
+type CropsRange struct {
+	AirTempMin   float64 `db:"air_temp_min"`
+	AirTempMax   float64 `db:"air_temp_max"`
+	HumidityMin  float64 `db:"humidity_min"`
+	HumidityMax  float64 `db:"humidity_max"`
+	PHLevelMin   float64 `db:"ph_level_min"`
+	PHLevelMax   float64 `db:"ph_level_max"`
+	OrpMin       float64 `db:"orp_min"`
+	OrpMax       float64 `db:"orp_max"`
+	TdsMin       uint16  `db:"tds_min"`
+	TdsMax       uint16  `db:"tds_max"`
+	WaterTempMin float64 `db:"water_temp_min"`
+	WaterTempMax float64 `db:"water_temp_max"`
+}
+
+func min(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func minInt(a, b uint16) uint16 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func maxInt(a, b uint16) uint16 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func (l *Notifications) GetAllByUser(uuid uint64) []Status {
+	var query = ""
+	status := []Status{}
+
+	query += "SELECT DISTINCT "
+	query += "greenhouses.guid, greenhouses.address, greenhouses.zip, greenhouses.display_name, greenhouses.status, greenhouses.destination, greenhouses.tempIn, greenhouses.tempOut, greenhouses.humidity, greenhouses.brightness, greenhouses.co2, "
+	query += "stacks.suid, "
+	query += "sprouts.sproutuid, sprouts.pH, sprouts.TDS, sprouts.ORP, sprouts.h2oTemp, sprouts.airTemp, sprouts.humidity, "
+	query += "pots.puid, "
+	query += "plants.crop_id, plants.nutrient_id, plants.pluid, plants.created_ts, plants.planted_ts, plants.harvested_ts, "
+	query += "crops.cuid, crops.crop_name, crops.air_temp_min, crops.air_temp_max, crops.humidity_min, crops.humidity_max, crops.ph_level_min, crops.ph_level_max, crops.orp_min, crops.orp_max, crops.tds_min, crops.tds_max, crops.water_temp_min, crops.water_temp_max "
+	query += "FROM users_greenhouses "
+	query += "JOIN greenhouses ON greenhouses.id = users_greenhouses.greenhouse_id "
+	query += "JOIN stacks ON greenhouses.id = stacks.greenhouse_id "
+	query += "JOIN sprouts ON stacks.id = sprouts.stack_id "
+	query += "JOIN pots ON stacks.id = pots.stack_id "
+	query += "JOIN plants ON pots.id = plants.pot_id "
+	query += "JOIN crops ON crops.id = plants.crop_id "
+	query += "WHERE users_greenhouses.user_id=?; "
+
+	/*
+		SELECT greenhouses.guid, greenhouses.address, greenhouses.zip, greenhouses.display_name, greenhouses.status, greenhouses.destination, greenhouses.tempIn, greenhouses.tempOut, greenhouses.humidity, greenhouses.brightness, greenhouses.co2, stacks.suid, sprouts.sproutuid, sprouts.pH, sprouts.TDS, sprouts.ORP, sprouts.h2oTemp, sprouts.airTemp, sprouts.humidity, pots.puid, plants.crop_id, plants.nutrient_id, plants.pluid, plants.created_ts, plants.planted_ts, plants.harvested_ts, crops.cuid, crops.crop_name, crops.air_temp_min, crops.air_temp_max, crops.humidity_min, crops.humidity_max, crops.ph_level_min, crops.ph_level_max, crops.orp_min, crops.orp_max, crops.tds_min, crops.tds_max, crops.water_temp_min, crops.water_temp_max FROM greenhouses JOIN stacks ON greenhouses.id = stacks.greenhouse_id JOIN sprouts ON stacks.id = sprouts.stack_id JOIN pots ON stacks.id = pots.stack_id JOIN plants ON pots.id = plants.pot_id JOIN crops ON crops.id = plants.crop_id;
+	*/
+	err := l.dbh.Select(&status, query, uuid)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	return status
+}
+
+func (l *Notifications) GetCropsSproutRange() *CropsRange {
+	var query = ""
+	crops := []Crop{}
+
+	query += "SELECT "
+	query += "id, cuid, crop_name, air_temp_min, air_temp_max, humidity_min, humidity_max, ph_level_min, ph_level_max, orp_min, orp_max, tds_min, tds_max, water_temp_min, water_temp_max "
+	query += "FROM crops; "
+	err := l.dbh.Select(&crops, query)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	cropsRange := CropsRange{
+		AirTempMin:   min(crops[0].AirTempMin, crops[1].AirTempMin),
+		AirTempMax:   max(crops[0].AirTempMax, crops[1].AirTempMax),
+		HumidityMin:  min(crops[0].HumidityMin, crops[1].HumidityMin),
+		HumidityMax:  max(crops[0].HumidityMax, crops[1].HumidityMax),
+		PHLevelMin:   min(crops[0].PHLevelMin, crops[1].PHLevelMin),
+		PHLevelMax:   max(crops[0].PHLevelMax, crops[1].PHLevelMax),
+		OrpMin:       min(crops[0].OrpMin, crops[1].OrpMin),
+		OrpMax:       min(crops[0].OrpMax, crops[1].OrpMax),
+		TdsMin:       minInt(crops[0].TdsMin, crops[1].TdsMin),
+		TdsMax:       maxInt(crops[0].TdsMax, crops[1].TdsMax),
+		WaterTempMin: min(crops[0].WaterTempMin, crops[1].WaterTempMin),
+		WaterTempMax: max(crops[0].WaterTempMax, crops[1].WaterTempMax),
+	}
+	return &cropsRange
+}
+
+type MergedCropsPerSprout struct {
+	SproutUID      map[uint64][]int
+	PH             float64 `db:"pH"`
+	TDS            int64   `db:"TDS"`
+	ORP            float64 `db:"ORP"`
+	WaterTemp      float64 `db:"h2oTemp"`
+	AirTemp        float64 `db:"airTemp"`
+	HumiditySprout float64 `db:"humidity"`
+}
+
+func groupIDsByValue(pairs []GetCropsPerSproutResult) []MergedCropsPerSprout {
+	merged := []MergedCropsPerSprout{}
+	result := make(map[uint64][]int)
+
+	for _, pair := range pairs {
+		result[pair.SproutUID] = append(result[pair.SproutUID], pair.CropID)
+		merged = append(merged, MergedCropsPerSprout{
+			SproutUID:      result,
+			PH:             pair.PH,
+			TDS:            pair.TDS,
+			ORP:            pair.ORP,
+			WaterTemp:      pair.WaterTemp,
+			AirTemp:        pair.AirTemp,
+			HumiditySprout: pair.HumiditySprout,
+		})
+	}
+
+	return merged
+}
+
+type GetCropsPerSproutResult struct {
+	CropID         int     `db:"crop_id"`
+	SproutUID      uint64  `db:"sproutuid"`
+	PH             float64 `db:"pH"`
+	TDS            int64   `db:"TDS"`
+	ORP            float64 `db:"ORP"`
+	WaterTemp      float64 `db:"h2oTemp"`
+	AirTemp        float64 `db:"airTemp"`
+	HumiditySprout float64 `db:"humidity"`
+}
+
+func (l *Notifications) GetCropsPerSprout(userID int) []GetCropsPerSproutResult {
+	var query = ""
+	crops := []GetCropsPerSproutResult{}
+	query += "SELECT DISTINCT "
+	query += "plants.crop_id, sprouts.sproutuid, sprouts.pH, sprouts.TDS, sprouts.ORP, sprouts.h2oTemp, sprouts.airTemp, sprouts.humidity "
+	query += "FROM users_greenhouses "
+	query += "JOIN greenhouses ON greenhouses.id = users_greenhouses.greenhouse_id "
+	query += "JOIN stacks ON greenhouses.id = stacks.greenhouse_id "
+	query += "JOIN sprouts ON stacks.id = sprouts.stack_id "
+	query += "JOIN pots ON stacks.id = pots.stack_id "
+	query += "JOIN plants ON pots.id = plants.pot_id "
+	query += "JOIN crops ON crops.id = plants.crop_id "
+	query += "WHERE users_greenhouses.user_id=?; "
+	err := l.dbh.Select(&crops, query, userID)
+	if err == sql.ErrNoRows {
+		return nil
+	}
+	return crops
+}
+
 // GetNotificationV1 gets pots by suid
 func (l *Notifications) CronNotification() error {
 	var err error
 
+	crops := l.GetCropsSproutRange()
+	cropsStack := l.GetCropsPerSprout(1)
+	mergedCropsStack := groupIDsByValue(cropsStack)
+
+	//for every merged crops stack check if current sprout values are in common with the crops sprout values
+	//if out of bound, insert in notifications
+	//if all in bound, return
+
+	fmt.Println(crops, cropsStack, mergedCropsStack)
 	c := cron.New()
 	cronID, err := c.AddFunc("@every 1s", func() {
 		var query = ""
 
-		notification := Status{}
+		notification := []Status{}
 
-		query += "SELECT "
-		query += "greenhouses.guid, greenhouses.address, greenhouses.zip, greenhouses.display_name, greenhouses.status, greenhouses.destination, greenhouses.tempIn, greenhouses.tempOut, greenhouses.humidity, greenhouses.brightness, greenhouses.co2, "
-		query += "stacks.suid, "
-		query += "sprouts.sproutuid, sprouts.pH, sprouts.TDS, sprouts.ORP, sprouts.h2oTemp, sprouts.airTemp, sprouts.humidity, "
-		query += "pots.puid, "
-		query += "plants.crop_id, plants.nutrient_id, plants.pluid, plants.created_ts, plants.planted_ts, plants.harvested_ts, "
-		query += "crops.cuid, crops.crop_name, crops.air_temp_min, crops.air_temp_max, crops.humidity_min, crops.humidity_max, crops.ph_level_min, crops.ph_level_max, crops.orp_min, crops.orp_max, crops.tds_min, crops.tds_max, crops.water_temp_min, crops.water_temp_max "
+		query += "SELECT DISTINCT "
+		query += "crops.cuid, crops.crop_name, crops.air_temp_min, crops.air_temp_max, crops.humidity_min, crops.humidity_max, crops.ph_level_min, crops.ph_level_max, crops.orp_min, crops.orp_max, crops.tds_min, crops.tds_max, crops.water_temp_min, crops.water_temp_max, "
+		query += "sprouts.sproutuid, sprouts.pH, sprouts.TDS, sprouts.ORP, sprouts.h2oTemp, sprouts.airTemp, sprouts.humidity "
 		query += "FROM users_greenhouses "
 		query += "JOIN greenhouses ON greenhouses.id = users_greenhouses.greenhouse_id "
 		query += "JOIN stacks ON greenhouses.id = stacks.greenhouse_id "
@@ -98,14 +283,12 @@ func (l *Notifications) CronNotification() error {
 		query += "JOIN pots ON stacks.id = pots.stack_id "
 		query += "JOIN plants ON pots.id = plants.pot_id "
 		query += "JOIN crops ON crops.id = plants.crop_id "
-		query += "WHERE users_greenhouses.user_id=?; "
+		query += "WHERE plants.planted_ts > 0 AND users_greenhouses.user_id=?; "
 
 		/*
 			SELECT greenhouses.guid, greenhouses.address, greenhouses.zip, greenhouses.display_name, greenhouses.status, greenhouses.destination, greenhouses.tempIn, greenhouses.tempOut, greenhouses.humidity, greenhouses.brightness, greenhouses.co2, stacks.suid, sprouts.sproutuid, sprouts.pH, sprouts.TDS, sprouts.ORP, sprouts.h2oTemp, sprouts.airTemp, sprouts.humidity, pots.puid, plants.crop_id, plants.nutrient_id, plants.pluid, plants.created_ts, plants.planted_ts, plants.harvested_ts, crops.cuid, crops.crop_name, crops.air_temp_min, crops.air_temp_max, crops.humidity_min, crops.humidity_max, crops.ph_level_min, crops.ph_level_max, crops.orp_min, crops.orp_max, crops.tds_min, crops.tds_max, crops.water_temp_min, crops.water_temp_max FROM greenhouses JOIN stacks ON greenhouses.id = stacks.greenhouse_id JOIN sprouts ON stacks.id = sprouts.stack_id JOIN pots ON stacks.id = pots.stack_id JOIN plants ON pots.id = plants.pot_id JOIN crops ON crops.id = plants.crop_id;
 		*/
-
-		//query = "SELECT nuid, created_ts, checked_ts, done_ts, title, message FROM notifications WHERE nuid=?;"
-		err = l.dbh.Get(&notification, query, 1)
+		err = l.dbh.Select(&notification, query, 1)
 		fmt.Println(notification, err)
 
 		if err == sql.ErrNoRows {
